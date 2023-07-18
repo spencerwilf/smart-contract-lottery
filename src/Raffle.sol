9f02ef7d68faf0307e9146adaaae67e89d7e95e3
Spencer Wilfahrt <spencerwilf@yahoo.com>
2023-07-17 18:56:47 -0700

    notice

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.19;
 
+/**
+ * @title Smart Contract Raffle
+ * @author Spencer Wilfahrt
+ * @notice This contract creates a provably random onchain lottery
+ */
+
 contract Raffle {}
