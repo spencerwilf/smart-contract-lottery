d1af3c88577dd1b4a780e6278506c80e49ac65e7
Spencer Wilfahrt <spencerwilf@yahoo.com>
2023-07-17 19:17:49 -0700

    custom entry error

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -7,4 +7,29 @@ pragma solidity ^0.8.19;
  * @notice This contract creates a provably random onchain lottery
  */
 
-contract Raffle {}
+contract Raffle {
+
+    error Raffle__NotEnoughEthSent();
+
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEthSent();
+        }
+    }
+
+    function pickWinner() public {
+
+    }
+
+    /** GETTER FUNCTIONS **/
+
+    function getEntranceFee() external view returns(uint256) {
+        return i_entranceFee;
+    }
+}
