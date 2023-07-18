04b1964abf950f7fe52db955a51ea5adf3f8b768
Spencer Wilfahrt <spencerwilf@yahoo.com>
2023-07-17 20:01:25 -0700

    enter raffle event

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -12,6 +12,11 @@ contract Raffle {
     error Raffle__NotEnoughEthSent();
 
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /** Events **/
+    event EnteredRaffle(address indexed player);
+    
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
@@ -21,6 +26,8 @@ contract Raffle {
         if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughEthSent();
         }
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
     }
 
     function pickWinner() public {
